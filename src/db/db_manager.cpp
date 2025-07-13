48f7c899f79fde9052c621259f00f8ce5fc12431
NickSkier <nickskier02@gmail.com>
2025-07-13 18:27:01 +0300

    Add: db method to populate db with `api/me` data

--- src/db/db_manager.cpp
+++ src/db/db_manager.cpp
@@ -74,5 +74,49 @@ void abscli::db::DbManager::updateUsersTableAfterLogin(const abscli::models::Use
     stmt.step(m_absclidb);
   } catch (const std::exception& e) {
     std::cerr << "\033[1;31mError:\033[0m Failed to insert user " << user.id << " after login: " << e.what() << "\n";
+
+auto abscli::db::DbManager::updateUsersTable(const abscli::models::User& user) -> void {
+  try {
+    Statement stmt(m_absclidb, "INSERT INTO users ("
+                               "id, username, absServer, "
+                               "email, type, "
+                               "seriesHideFromContinueListening, "
+                               "bookmarks, isActive, isLocked, "
+                               "lastSeen, createdAt, "
+                               "permissions, librariesAccessible, "
+                               "itemTagsSelected) "
+                               "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "
+                               "ON CONFLICT(id) DO UPDATE SET "
+                               "username = excluded.username, "
+                               "absServer = excluded.absServer, "
+                               "email = excluded.email, "
+                               "type = excluded.type, "
+                               "seriesHideFromContinueListening = excluded.seriesHideFromContinueListening, "
+                               "bookmarks = excluded.bookmarks, "
+                               "isActive = excluded.isActive, "
+                               "isLocked = excluded.isLocked, "
+                               "lastSeen = excluded.lastSeen, "
+                               "createdAt = excluded.createdAt, "
+                               "permissions = excluded.permissions, "
+                               "librariesAccessible = excluded.librariesAccessible, "
+                               "itemTagsSelected = excluded.itemTagsSelected;"
+    );
+    stmt.bind(1, user.id);
+    stmt.bind(2, user.username);
+    stmt.bind(3, user.absServer);
+    stmt.bind(4, user.email);
+    stmt.bind(5, user.type);
+    stmt.bind(6, user.seriesHideFromContinueListening);
+    stmt.bind(7, user.bookmarks);
+    stmt.bind(8, user.isActive);
+    stmt.bind(9, user.isLocked);
+    stmt.bind(10, user.lastSeen);
+    stmt.bind(11, user.createdAt);
+    stmt.bind(12, user.permissions);
+    stmt.bind(13, user.librariesAccessible);
+    stmt.bind(14, user.itemTagsSelected);
+    stmt.step(m_absclidb);
+  } catch (const std::exception& e) {
+    std::cerr << "\033[1;31m[ERROR]\033[0m Failed to insert user " << user.id << ": " << e.what() << "\n";
   }
 }
